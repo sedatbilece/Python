b3c19b8ebe9c9661d14e184239a1654ad5ee197e
sedat <sedatb767@gmail.com>
2020-09-14 17:38:43 +0300

    Create random.py

---
+++ random.py
@@ -0,0 +1,20 @@
+import random
+
+
+random.randrange(1, 100,5)# 1 ile 100 araasında 5 arttırarak sayı oluşturur 5k+1 şeklinde
+
+random.randint(5,25)#  x1<= x <=x2  rastgele int üretir
+
+random.choice([1,2,3,4,5,6])# dizinin içinden rastgele bir eleman döndürür
+
+
+random.random()# 0.0 ile 1.0 arasında float dönderir
+
+random.triangular(1.0,5.0)# istenen aralıkta float döndürür
+ 
+
+x=[1,2,3,4,5]
+random.shuffle(x)# rastgele diziyi karıştırır
+x
+
+
