c5289d21f513c07bac60b97ae3656c8664c0ee57
sedat <sedatb767@gmail.com>
2020-09-18 22:42:06 +0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,44 @@
+import math 
+
+
+math.floor(2.9)#  2 ||alt degere yuvarlar
+
+ math.factorial(5) # 120 ||faktoriyel
+
+ math.fabs(-4) #   4.0   ||mutlak deger gibi
+
+ abs(-9) # 9  || mutlak deger
+
+ math.copysign(5.0, -2.0) # 5.0  || dönüs degeri ilk eleman işareti 2. elemandan alır
+
+math.fmod(8,5)  # 3.0 || 8%5 yapar 8 in 5 ile bölümünden kalan gibi
+
+sum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1])#0.9999999999999999
+math.fsum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1])#1.0 || yuvarlar
+
+math.gcd(72,16) # 8 || en büyük ortak bölen
+
+math.isfinite(4) # true || sonsuz mu diye bakar bool döndürür
+
+math.isnan(2)# false || sayı değilmi diye bakar değilse true döndürür
+
+math.ldexp(5,2)#20 || (x1,x2)  x1*(2**x2) döndürür
+
+pow(2,6)# 64 || üs alma
+
+math.exp(5) #148.41315  || e^5 döndürür
+
+math.log(100,10)#2.0 || log 10 tabanında 100 işelmini döndürür
+
+math.sqrt(49)# 7 || karesi verilen sayı olan sayıyı döndürür
+
+math.e#2.718281828459045
+
+math.pi#3.141592653589793
+
+
+
+
+
+
+
